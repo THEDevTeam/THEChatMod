cb376e7664fb565c0035d4fa80200af0f6c0aa6e
Rob4001 <robert.wilson1717@gmail.com>
2012-04-05 17:54:41 +0100

    Initial Commit and pom config

---
+++ src/main/java/com/thedevteam/thechatmod/TheChatMod.java
@@ -0,0 +1,24 @@
+package com.thedevteam.thechatmod;
+
+import org.spout.api.plugin.CommonPlugin;
+
+/**
+ * 
+ *
+ */
+public class TheChatMod extends CommonPlugin 
+{
+
+	@Override
+	public void onDisable() {
+		throw new UnsupportedOperationException("Not done yet!");
+		
+	}
+
+	@Override
+	public void onEnable() {
+		throw new UnsupportedOperationException("Not done yet!");
+		
+	}
+
+}
