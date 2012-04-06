c15d3262aeb603880a3325b1af4c3c636736e0b9
dvdbrander <dvdbrander@gmail.com>
2012-04-06 12:19:24 +0200

    Made it create the config file if it didn't exist.

--- src/main/java/com/thedevteam/thechatmod/TheChatMod.java
+++ src/main/java/com/thedevteam/thechatmod/TheChatMod.java
@@ -1,6 +1,7 @@
 package com.thedevteam.thechatmod;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 
 import org.spout.api.plugin.CommonPlugin;
@@ -25,7 +26,21 @@ public class TheChatMod extends CommonPlugin {
 	@Override
 	public void onEnable() {
 		this.cm = new ChannelManager(this);
-		this.config = new YamlConfiguration(new File(this.getDataFolder(),"config.yml"));
+
+		if (!this.getDataFolder().exists()) {
+			this.getDataFolder().mkdir();
+		}
+
+		File config = new File(this.getDataFolder(), "config.yml");
+		if (!config.exists()) {
+			try {
+				config.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		this.config = new YamlConfiguration(config);
+
 		getGame().getEventManager().registerEvents(cm, this);
 		log(Level.INFO, "THEChatMod has been enabled");
 	}
@@ -33,8 +48,8 @@ public class TheChatMod extends CommonPlugin {
 	public void log(Level warning, String string) {
 		getLogger().log(warning, string);
 	}
-	
-	public YamlConfiguration getConfig(){
+
+	public YamlConfiguration getConfig() {
 		return config;
 	}
 
