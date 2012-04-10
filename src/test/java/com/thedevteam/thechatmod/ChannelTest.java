47eb61f64f68434f47035a8e23390a6fed4506e6
Rob4001 <robert.wilson1717@gmail.com>
2012-04-10 13:46:46 +0100

    Fix ChannelTest

--- src/test/java/com/thedevteam/thechatmod/ChannelTest.java
+++ src/test/java/com/thedevteam/thechatmod/ChannelTest.java
@@ -3,7 +3,9 @@ package com.thedevteam.thechatmod;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.spout.api.util.config.Configuration;
 import org.spout.api.util.config.ConfigurationHolder;
+import org.spout.api.util.config.MemoryConfiguration;
 
 import com.thedevteam.thechatmod.Channels.Channel;
 
@@ -11,13 +13,15 @@ public class ChannelTest {
 	
 	@Test
 	public void newChannelTest(){
-		ConfigurationHolder config = new ConfigurationHolder("test","name");
-		ConfigurationHolder le = new ConfigurationHolder("local.enable","false");
-		ConfigurationHolder ld = new ConfigurationHolder("local.distance","100");
-		config.addChild(le);
-		config.addChild(ld);
+		Configuration config = new MemoryConfiguration();
+		ConfigurationHolder chcfg = new ConfigurationHolder("test","name");
+		chcfg.setConfiguration(config);
+		ConfigurationHolder le = new ConfigurationHolder("false","local","enable");
+		ConfigurationHolder ld = new ConfigurationHolder("100","local","distance");
+		chcfg.addChild(le);
+		chcfg.addChild(ld);
 		
-		Channel test = new Channel("test", config);
+		Channel test = new Channel("test", chcfg);
 		
 		assertTrue(test.getName() == "test");
 		assertEquals(test.isLocal(),false);
